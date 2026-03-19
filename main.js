665ae6d776dfe9eb91db5905fc4a6ef94868211d
Matt <matthew@adler.id.au>
2026-03-20 10:37:58 +1100

    Task: Call generation every 5 seconds for demo purpose (until controls are added)

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ import * as THREE from 'three';
 import { setupScene } from './setup.js';
 import { doProceduralGeneration } from './generation.js';
 
+const windowVariance = 0.8;
+const barDisplacement = 0.3;
+
 setupScene();
-doProceduralGeneration(0.8, 0.3);
+doProceduralGeneration(windowVariance, barDisplacement);
+const callGen = () => doProceduralGeneration(windowVariance, barDisplacement);
+
+setInterval(callGen, 5000);
