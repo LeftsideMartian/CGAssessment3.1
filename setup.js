d2ba46ee351a8add21ce2d2477cdbb275acf4f9b
Matt <matthew@adler.id.au>
2026-03-19 15:55:43 +1100

    Task: Setup changes

--- setup.js
+++ setup.js
@@ -10,7 +10,6 @@ export let scene;
 export let camera;
 export let renderer;
 export const CLOCK = new THREE.Clock();
-export let cube;
 
 //Constants/values
 const floorYValue = -5;
@@ -23,6 +22,8 @@ export const setupScene = () => {
 };
 
 export function setScene() {
+	const cameraStartPosValue = 20;
+
 	// Create and configure scene object
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color('#17171a');
@@ -31,7 +32,11 @@ export function setScene() {
 	const renderView = document.querySelector('.render-view');
 	const aspectRatio = renderView.clientWidth / renderView.clientHeight;
 	camera = new THREE.PerspectiveCamera(45, aspectRatio, 0.1, 1000);
-	camera.position.set(15, 15, 15);
+	camera.position.set(
+		cameraStartPosValue,
+		cameraStartPosValue,
+		cameraStartPosValue,
+	);
 	camera.lookAt(0, 0, 1);
 
 	// Create and configure renderer
@@ -43,9 +48,8 @@ export function setScene() {
 
 const setSceneElements = () => {
 	createFloor();
-	// createBuildingBase();
-	createTempBuilding();
-	bevelTest();
+	createBuildingBase();
+	createBuilding();
 };
 
 const createFloor = () => {
@@ -72,8 +76,9 @@ const createFloor = () => {
 const createBuildingBase = () => {
 	//Constants
 	const baseHeight = 2;
+	const baseWidth = 15;
 
-	const baseGeometry = new THREE.BoxGeometry(15, baseHeight, 15);
+	const baseGeometry = new THREE.BoxGeometry(baseWidth, baseHeight, baseWidth);
 	const baseMaterial = new THREE.MeshBasicMaterial({
 		map: gravelConcreteTexture,
 	});
@@ -85,52 +90,24 @@ const createBuildingBase = () => {
 	scene.add(buildingBase);
 };
 
-const createTempBuilding = () => {
-	let cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-	let cubeMaterial = new THREE.MeshBasicMaterial({
+const createBuilding = () => {
+	//Constants
+	const buildingHeight = 15;
+	const buildingWidth = 5;
+
+	let buildingGeometry = new THREE.BoxGeometry(
+		buildingWidth,
+		buildingHeight,
+		buildingWidth,
+	);
+	let buildingMaterial = new THREE.MeshBasicMaterial({
 		color: new THREE.Color(0, 1, 0),
 		wireframe: true,
 	});
 
-	cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+	const building = new THREE.Mesh(buildingGeometry, buildingMaterial);
 
-	cube.position.y = floorYValue + 2.5;
+	building.position.y = floorYValue + buildingHeight / 2 + 2;
 
-	scene.add(cube);
-};
-
-const bevelTest = () => {
-	const unitSize = 15;
-
-	// Create a 2D shape that is a square
-	const bevelShape = new THREE.Shape();
-	bevelShape.moveTo(0, unitSize);
-	bevelShape.lineTo(unitSize, unitSize);
-	bevelShape.lineTo(unitSize, 0);
-	bevelShape.lineTo(0, 0);
-
-	// Define extrusion settings with bevel properties enabled
-	var extrudeSettings = {
-		steps: 1, // Number of steps along the depth
-		depth: 2, // The depth of the extrusion
-		bevelEnabled: true, // Enable the bevel
-		bevelThickness: 0.5, // How deep the bevel goes into the shape
-		bevelSize: 0.4, // How wide the bevel is
-		bevelOffset: 0, // Where the bevel starts
-		bevelSegments: 1, // How smooth the bevel is
-	};
-
-	var baseGeometry = new THREE.ExtrudeGeometry(bevelShape, extrudeSettings);
-	var baseMaterial = new THREE.MeshBasicMaterial({
-		map: gravelConcreteTexture,
-	});
-
-	var buildingBase = new THREE.Mesh(baseGeometry, baseMaterial);
-
-	buildingBase.position.y = -3.5;
-	buildingBase.position.x -= unitSize / 2;
-	buildingBase.position.z -= unitSize / 2;
-	buildingBase.rotation.x = Math.PI / 2;
-
-	scene.add(buildingBase);
+	scene.add(building);
 };
