a77685810c38971b0b14b7724cc96da7bcf3f2a2
Matt <matthew@adler.id.au>
2026-03-20 10:31:45 +1100

    Feat: Rework window generation algorithm and create window bars

--- generation.js
+++ generation.js
@@ -7,104 +7,46 @@ const windowInnerRadius = windowRadius / 6;
 
 const getRandomValue = (max) => Math.random() * max;
 
-export const doProceduralGeneration = () => {
+export const doProceduralGeneration = (windowVariance, barDisplacement) => {
 	// Clear any existing children of the building object first
 	building.clear();
 
 	// Then generate windows on each face
-	generatePosZWindows();
-	generateNegZWindows();
-	generatePosXWindows();
-	generateNegXWindows();
+	generateWindows(windowVariance, barDisplacement);
 };
 
-const generatePosZWindows = () => {
-	// 2d for loop to loop over all possible window placements in the grid
-	for (let i = -4; i < 6; i++) {
-		for (let j = 0; j < 3; j++) {
-			if (Math.random() >= 0.8) {
-				const newWindow = createWindow();
+const generateWindows = (windowVariance, barDisplacement) => {
+	const templateWindow = createWindow();
 
-				// Rotate window to line up with the building correctly
+	// Loop through each face of the building (0-3: PosZ, PosX, NegZ, NegX)
+	for (let sideIndex = 0; sideIndex < 4; sideIndex++) {
+		// 2d for loop for grid positions on this face
+		for (let gridY = -4; gridY < 6; gridY++) {
+			for (let gridX = 0; gridX < 3; gridX++) {
+				if (getRandomValue(1) >= windowVariance) {
+					let newWindow = templateWindow.clone();
+					newWindow = createWindowBars(newWindow, barDisplacement);
 
-				// Align window with building face
-				newWindow.position.z += buildingWidth / 2;
+					// Rotate at origin based on which face (0, π/2, π, 3π/2)
+					newWindow.rotation.y = (Math.PI / 2) * sideIndex;
 
-				// Adjust x and y pos based on loop
-				newWindow.position.x -= windowSpaceValue; // Left align all windows first
-				newWindow.position.x += windowSpaceValue * j; // Then change x pos based on loop
-				newWindow.position.y = windowSpaceValue * i; // Set y pos based on loop too
+					// Position based on which face
+					newWindow.position.y = windowSpaceValue * gridY;
 
-				building.add(newWindow);
-			}
-		}
-	}
-};
+					if (sideIndex % 2 === 0) {
+						// PosZ (0) or NegZ (2)
+						newWindow.position.z =
+							(sideIndex === 0 ? 1 : -1) * (buildingWidth / 2);
+						newWindow.position.x = -windowSpaceValue + windowSpaceValue * gridX;
+					} else {
+						// PosX (1) or NegX (3)
+						newWindow.position.x =
+							(sideIndex === 1 ? 1 : -1) * (buildingWidth / 2);
+						newWindow.position.z = -windowSpaceValue + windowSpaceValue * gridX;
+					}
 
-const generateNegZWindows = () => {
-	// 2d for loop to loop over all possible window placements in the grid
-	for (let i = -4; i < 6; i++) {
-		for (let j = 0; j < 3; j++) {
-			if (Math.random() >= 0.8) {
-				const newWindow = createWindow();
-
-				// Align window with building face
-				newWindow.position.z -= buildingWidth / 2;
-
-				// Adjust x and y pos based on loop
-				newWindow.position.x -= windowSpaceValue; // Left align all windows first
-				newWindow.position.x += windowSpaceValue * j; // Then change x pos based on loop
-				newWindow.position.y = windowSpaceValue * i; // Set y pos based on loop too
-
-				building.add(newWindow);
-			}
-		}
-	}
-};
-
-const generatePosXWindows = () => {
-	// 2d for loop to loop over all possible window placements in the grid
-	for (let i = -4; i < 6; i++) {
-		for (let j = 0; j < 3; j++) {
-			if (Math.random() >= 0.8) {
-				const newWindow = createWindow();
-
-				// Rotate window to line up with the building correctly
-				newWindow.rotation.y = Math.PI / 2;
-
-				// Align window with building face
-				newWindow.position.x += buildingWidth / 2;
-
-				// Adjust y and x pos based on loop
-				newWindow.position.z -= windowSpaceValue; // Left align all windows first
-				newWindow.position.z += windowSpaceValue * j; // Then change x pos based on loop
-				newWindow.position.y = windowSpaceValue * i; // Set y pos based on loop too
-
-				building.add(newWindow);
-			}
-		}
-	}
-};
-
-const generateNegXWindows = () => {
-	// 2d for loop to loop over all possible window placements in the grid
-	for (let i = -4; i < 6; i++) {
-		for (let j = 0; j < 3; j++) {
-			if (Math.random() >= 0.8) {
-				const newWindow = createWindow();
-
-				// Rotate window to line up with the building correctly
-				newWindow.rotation.y = Math.PI / 2;
-
-				// Align window with building face
-				newWindow.position.x -= buildingWidth / 2;
-
-				// Adjust y and x pos based on loop
-				newWindow.position.z -= windowSpaceValue; // Left align all windows first
-				newWindow.position.z += windowSpaceValue * j; // Then change x pos based on loop
-				newWindow.position.y = windowSpaceValue * i; // Set y pos based on loop too
-
-				building.add(newWindow);
+					building.add(newWindow);
+				}
 			}
 		}
 	}
@@ -114,7 +56,7 @@ const createWindow = () => {
 	// Geometry - TorusGeometry to create a window with thickness instead of RingGeometry which I also tried
 	const windowGeometry = new THREE.TorusGeometry(
 		windowRadius,
-		windowRadius / 6,
+		windowInnerRadius,
 		6,
 		4,
 	);
@@ -128,4 +70,30 @@ const createWindow = () => {
 	newWindow.rotation.z = Math.PI / 4;
 
 	return newWindow;
+};
+
+// Mutating method to add 2 randomly placed bars to the window
+// barDisplacement is a value between 0 and 1 that determines how randomly rotated the window bars should be
+const createWindowBars = (window, barDisplacement) => {
+	const barThickness = 0.12;
+
+	for (let i = 0; i < 2; i++) {
+		const randomPosOrNeg = getRandomValue(1) >= 0.5 ? 1 : -1; // 50/50 between positive and negative
+		const randomValue = getRandomValue(barDisplacement) * randomPosOrNeg;
+
+		// Geometry
+		const barGeometry = new THREE.BoxGeometry(barThickness, 1, barThickness);
+		// Material
+		const barMaterial = new THREE.MeshBasicMaterial({
+			color: 0xffe000,
+		});
+		// Objects
+		const windowBar = new THREE.Mesh(barGeometry, barMaterial);
+
+		windowBar.rotation.z = ((i === 0 ? 1 : -1) * Math.PI) / 4 + randomValue;
+
+		window.add(windowBar);
+	}
+
+	return window;
 };
